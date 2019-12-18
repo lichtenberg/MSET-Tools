c5dfc65b9b7e33f45597aa8473d9b992010b79a6
Mitch Lichtenberg <mpl@apple.com>
2019-12-18 15:01:40 -0800

    Much revamping to use the 'promise' callback stuff

--- docs/toolusers.js
+++ docs/toolusers.js
@@ -1,46 +1,56 @@
 
-
+//
+// Dig the token out of our URL parameters.
+//
 var urlParams = new URLSearchParams(window.location.search)
-
 var accessToken = urlParams.get('access_token')
 
 console.log(`Our access token is ${accessToken}`)
 
+//
+// Create a github object and get a handle on our repository.
+//
+
 var gh = new GitHub({
-     token: accessToken
+    token: accessToken
      });
 
 var repo = gh.getRepo('lichtenberg','MSET-Tools');
 
+//
+// Globals
+//
+var table = null;
 var toolList = [];
 
-repo.getContents("master","ToolNames.json",true,
-                 function(err,stuff) {
-                     toolList = stuff;
-                 }
-                );
-
-console.log(toolList);
-
-
-var table = new Tabulator("#example-table", {
-        //height:"311px",
-    columns:[
-        {formatter:"rowSelection", titleFormatter:"rowSelection", align:"center", headerSort:false, cellClick:function(e, cell){
+//
+// Static columns (always present)
+//
+var staticColumns = [
+    {formatter:"rowSelection", titleFormatter:"rowSelection", align:"center", frozen:true,
+     headerSort:false, cellClick:function(e, cell){
            cell.getRow().toggleSelect();
            }},
-        {title:"Name", field:"name", width:200, editor:"input"},
-        {title:"Active", field:"active", formatter:"tickCross", editor:true},
-        {title:"Mentor", field:"mentor", formatter:"tickCross", editor:true},
-        {title:"Badge#", field:"badge", editor:"input"},
-        {title:"Band Saw", field:"bandsaw",formatter:"tickCross", editor:true},
-        {title:"Drill Press", field:"drillpress", formatter:"tickCross",editor:"tickCross"},
-        {title:"Miter Saw", field:"mitersaw", formatter:"tickCross",editor:"tickCross"},
-        {title:"Lathe", field:"lathe", formatter:"tickCross",editor:"tickCross"},
-        {title:"CNC Router", field:"cncrouter", formatter:"tickCross",editor:"tickCross"},
-    ],
-});
+    {title:"Name", field:"name", width:200, editor:"input",frozen:true},
+    {title:"Active", field:"active", formatter:"tickCross", editor:true},
+    {title:"Mentor", field:"mentor", formatter:"tickCross", editor:true}
+];
 
+
+//
+// Generate column info for tabulator given a tool list we got from github
+//
+function generateColumns(tools)
+{
+    columns = staticColumns;
+
+    tools.forEach(function(v) {
+        e = { title: v.name, field: v.key, formatter:"tickCross", editor:true};
+        columns.push(e);
+    })
+
+    return columns;
+}
 
 //Add row on "Add Row" button click
 $("#add-user").click(function(){
@@ -67,6 +77,9 @@ $("#commit-changes").click(function(){
 });
 
 
+//
+// commit current contents of table to github
+//
 function commitToGithub()
 {
 
@@ -93,6 +106,11 @@ function commitToGithub()
 
 }
 
+
+//
+// Retrieve the table info from github
+// not used anymore, we do the promise thing
+//
 function loadTableFromGithub() {
 
     repo.getContents("master","AuthorizedUsers.txt",true,
@@ -104,5 +122,27 @@ function loadTableFromGithub() {
 }
 
 
-loadTableFromGithub();
+
+
+//
+// Mainline code: Retrieve the tool names, then the table of authorizations
+//
+repo.getContents("master","ToolNames.json",true)
+    .then(function(result) {
+        table = new Tabulator("#example-table", { columns:generateColumns(result.data) })
+        return result;
+    })
+    .then(function(result) {
+        return repo.getContents("master","AuthorizedUsers.txt",true)                                
+    })
+    .then(function(result) {
+        table.setData(result.data);
+        return result;
+    })
+    .catch(function(err) {
+        console.log(`Error callback : ${err.message}`);
+        return err;
+    });
+
+
 
